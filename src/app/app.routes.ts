18f41b907a1018eb9220e8a1d2d884b05759a2ba
Andr3wZSM <andrew.malayko@gmail.com>
2024-09-24 13:00:09 +0300

    added rooting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/play/play.component';
+import { FinishComponent } from './pages/finish/finish.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: 'home', component: HomeComponent },
+   { path: 'play', component: PlayComponent },
+   { path: 'finish', component: FinishComponent },
+   { path: '', redirectTo: 'home', pathMatch: 'full' },
+   { path: '**', component: PageNotFoundComponent }
+];
