4ba50bb1c91d6706d60f0e01f9b98f7027e854e9
Andr3wZSM <andrew.malayko@gmail.com>
2024-09-28 15:30:58 +0300

    added i'm lucky button and its logic

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { QuizService } from '../../services/quiz.service';
 import { CommonModule } from '@angular/common';
 import { Quiz } from '../../models/quiz.model';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +14,18 @@ import { RouterLink } from '@angular/router';
 export class HomeComponent implements OnInit {
   quizzes: Quiz[] = [];
 
-  constructor(private quizService: QuizService) { }
+  constructor(private quizService: QuizService, private router: Router) { }
 
   ngOnInit(): void {
     this.quizService.fetchQuestions().subscribe({
       next: (data) => this.quizzes = this.quizService.buildQuizzes(data.results),
       error: (error) => console.error('Error fetching questions: ', error)
     });
+  }
+
+  public playRandomQuiz(): void {
+    const randomIndex = Math.floor(Math.random() * this.quizzes.length);
+    const randomQuizId = this.quizzes[randomIndex].id;
+    this.router.navigate(['/play', randomQuizId]);
   }
 }
